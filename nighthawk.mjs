1354d260e8925abd9f0e13fee9dac17a46704372
nighthawk <sucklyf@yahoo.com>
2024-08-16 02:12:39 +0300

    Update nighthawk.mjs

--- nighthawk.mjs
+++ nighthawk.mjs
@@ -4,29 +4,38 @@ import extractJsonFromString from "extract-json-from-string";
 import { token, channelId, serverId, gateway, address } from "./cfg.mjs";
 
 const guilds = {};
-let vanity, tlsSocket, websocket;
+let vanity;
 
-const connectWebSocket = () => {
-    websocket = new WebSocket(gateway);
-    websocket.onclose = () => connectWebSocket();
-    websocket.onmessage = async ({ data }) => {
-        const { d, op, t } = JSON.parse(data);
-        if (t === "READY") d.guilds.forEach(g => g.vanity_url_code && (guilds[g.id] = g.vanity_url_code));
-        if (op === 10) websocket.send(JSON.stringify({ op: 2, d: { token, intents: 1 << 0, properties: { os: "linux", browser: "firefox", device: "nighthawk" }}}));
-        if (t === "GUILD_UPDATE" && guilds[d.guild_id] && guilds[d.guild_id] !== d.vanity_url_code) {
-            const find = guilds[d.guild_id];
-            tlsSocket.write(`PATCH /api/v9/guilds/${serverId}/vanity-url HTTP/1.1\r\nHost: ${address}\r\nAuthorization: ${token}\r\nContent-Length: ${JSON.stringify({ code: find }).length}\r\n\r\n${JSON.stringify({ code: find })}`);
-            vanity = find;
-        }
+const connectWebSocket = (ws) => ws.onopen = () => ws.send(JSON.stringify({
+  op: 2, t: "0.001", d: { op: 0, token, intents: 513, properties: { os: "Linux", browser: "Firefox", device: "Nighthawk" } }
+}));
+
+const connect = () => {
+  const ws = new WebSocket(gateway);
+  const socket = tls.connect({ host: address, port: 443 });
+
+  ws.onclose = connect;
+  ws.onmessage = ({ data }) => {
+    const { d, op, t } = JSON.parse(data);
+    if (t === "READY") {
+      d.guilds.forEach(g => g.vanity_url_code && (guilds[g.id] = g.vanity_url_code));
+      console.log(guilds);
+    } else if (op === 10) connectWebSocket(ws);
+    else if (t === "GUILD_UPDATE" && guilds[d.guild_id] && guilds[d.guild_id] !== d.vanity_url_code) {
+      const code = guilds[d.guild_id];
+      socket.write(`PATCH /api/v9/guilds/${serverId}/vanity-url HTTP/1.1\r\nHost: ${address}\r\nAuthorization: ${token}\r\nContent-Type: application/json\r\nContent-Length: ${Buffer.byteLength(JSON.stringify({ code }))}\r\n\r\n${JSON.stringify({ code })}`);
+      vanity = code;
     }
-}
-const connectTLS = () => {
-    tlsSocket = tls.connect({ host: address, port: 443 });
-    tlsSocket.on("secureConnect", connectWebSocket);
-    tlsSocket.on("data", async data => {
-        const jsonObject = extractJsonFromString(data.toString()).find(obj => obj.code || obj.message);
-        if (jsonObject) tlsSocket.write(`POST /api/v9/channels/${config.channelId}/messages HTTP/1.1\r\nHost: ${config.address}\r\nAuthorization: ${config.token}\r\nContent-Length: ${Buffer.byteLength(JSON.stringify({ content: `@everyone *Vanity Code : \`${vanity}\`\nValue : \`${JSON.stringify(jsonObject)}\`*` }))}\r\n\r\n${JSON.stringify({ content: `@everyone *Code : \`${vanity}\`\nUses: \`${JSON.stringify(jsonObject)}\`*` })}`);
-    });
+  };
+
+  socket.on("secureConnect", () => connectWebSocket(ws));
+  socket.on("data", (data) => {
+    const jsonObject = extractJsonFromString(data.toString()).find(obj => obj.code || obj.message);
+    if (jsonObject && ws.readyState === WebSocket.OPEN) {
+      console.log(jsonObject);
+      ws.send(JSON.stringify({ content: `*Code: \`${vanity}\`\nUses: \`${JSON.stringify(jsonObject)}\`*` }));
+    }
+  });
 };
 
-connectTLS();
+connect();
